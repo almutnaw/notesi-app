8495938ba5b8a6f75df5c852105c2679e11d40ef
Nawwaf <almutnaw@onid.orst.edu>
2015-10-17 21:26:44 -0700

    Starting the app

--- Notesi/ViewController.m
+++ Notesi/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    printf("I can't wait to push this project to GitHub");
 }
 
 - (void)didReceiveMemoryWarning {
