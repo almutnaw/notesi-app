9025d27886b3d5ff75f03422eb5fe51c98c45cd7
Nawwaf <almutnaw@onid.orst.edu>
2015-10-17 21:23:35 -0700

    Initial Commit

---
+++ Notesi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Notesi
+//
+//  Created by Nawwaf Almutairi on 10/17/15.
+//  Copyright (c) 2015 Nawwaf. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
